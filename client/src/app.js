68ace24d2028feda3a34db7e09a36d9c881bdc4e
Alison Wood <alisonwood29@gmail.com>
2018-02-28 19:24:17 +0000

    can populate dropdown with countries in app.js

--- client/src/app.js
+++ client/src/app.js
@@ -1,5 +1,23 @@
+const BucketListView = require('./views/bucketListView');
+const Request = require('./services/request.js');
+
+const bucketListView = new BucketListView();
+const countriesRequest = new Request('https://restcountries.eu/rest/v2/all');
+
+
 const app = function () {
-  
+  countriesRequest.get(populateDropDown);
+
+}
+
+const populateDropDown = function (countries) {
+  const select = document.getElementById('country');
+
+  countries.forEach(function (country) {
+    const option = document.createElement('option');
+    option.innerText = country.name;
+    select.appendChild(option);
+  })
 }
 
 document.addEventListener('DOMContentLoaded', app);
